711b7fe8b26b8fb0b6dbd52d72adf82dc7d4dbe4
baipengyan <baipengyan0101@qq.com>
2025-02-20 16:54:04 +0800

    🎉 开始一个项目 还等什么啊骚年，上号！
    Signed-off-by: baipengyan <baipengyan0101@qq.com>

--- OmniCore-authorization-server/src/main/java/com/beloved/omnicoreauthorizationserver/OmniCoreAuthorizationServerApplication.java
+++ OmniCore-authorization-server/src/main/java/com/beloved/omnicoreauthorizationserver/OmniCoreAuthorizationServerApplication.java
@@ -3,11 +3,14 @@ package com.beloved.omnicoreauthorizationserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author baipengyan
+ */
 @SpringBootApplication
 public class OmniCoreAuthorizationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OmniCoreAuthorizationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OmniCoreAuthorizationServerApplication.class, args);
+    }
 
 }
--- OmniCore-authorization-server/src/main/java/com/beloved/omnicoreauthorizationserver/config/SecurityConfig.java
+++ OmniCore-authorization-server/src/main/java/com/beloved/omnicoreauthorizationserver/config/SecurityConfig.java
@@ -1,4 +1,207 @@
 package com.beloved.omnicoreauthorizationserver.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.UUID;
+
+/**
+ * @author baipengyan
+ */
+@Configuration
 public class SecurityConfig {
+
+    /**
+     * 启动时生成的密钥，用于创建上面的JWKSource
+     *
+     * @return {@link KeyPair }
+     */
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) ->
+                        authorize.anyRequest().authenticated()
+                )
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                );
+        http
+                .getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    /**
+     * 用于检索用户进行身份验证
+     *
+     * @return {@link UserDetailsService }
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("admin")
+                .password(passwordEncoder().encode("123123"))
+                .roles("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * 用于密码加密
+     *
+     * @return {@link PasswordEncoder }
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 用于管理客户端
+     *
+     * @return {@link RegisteredClientRepository }
+     */
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        TokenSettings tokenSettings = TokenSettings.builder()
+                // 设置访问令牌有效期为1小时
+                .accessTokenTimeToLive(Duration.ofHours(1))
+                // 设置刷新令牌有效期为30天
+                .refreshTokenTimeToLive(Duration.ofDays(30))
+                // 这个设置是开启不透明token
+                //.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                // 使用透明token
+                .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                .build();
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("chick")
+                .clientSecret(passwordEncoder().encode("123456"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("https://www.baidu.com")
+                .postLogoutRedirectUri("http://127.0.0.1:8000/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .tokenSettings(tokenSettings)
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .build())
+                .build();
+        return new InMemoryRegisteredClientRepository(oidcClient);
+    }
+
+    /**
+     * 用于签署访问令牌
+     *
+     * @return {@link JWKSource }<{@link SecurityContext }>
+     */
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    /**
+     * 用于解码签名访问令牌
+     *
+     * @param jwkSource JSON Web 密钥 （JWK） 源
+     * @return {@link JwtDecoder }
+     */
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+
+    /**
+     * 用于配置 Spring Authorization Server
+     *
+     * @return {@link AuthorizationServerSettings }
+     */
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+
 }
